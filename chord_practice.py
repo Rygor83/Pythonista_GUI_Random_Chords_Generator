497f7a5a692340ff8a1f9e7499376a953541aa25
Rygor <Rygor83@users.noreply.github.com>
2022-03-25 13:37:31 +0300

    0.9. caption for tableviews

--- chord_practice.py
+++ chord_practice.py
@@ -196,6 +196,7 @@ screen_width = ui.get_screen_size().width
 screen_height = ui.get_screen_size().height
 
 tv_height = 125
+tv_width = screen_width / 3
 tv_x_pos = screen_width / 3
 tv_y_pos = 0
 
@@ -204,7 +205,7 @@ tv_alter = ui.TableView()
 tv_alter.border_width = 0
 tv_alter.x = 0 * tv_x_pos
 tv_alter.y = tv_y_pos
-tv_alter.width = screen_width / 3
+tv_alter.width = tv_width
 tv_alter.height = tv_height
 
 # Table view: Chord type
@@ -212,7 +213,7 @@ tv_type = ui.TableView()
 tv_type.border_width = 0
 tv_type.x = 1 * tv_x_pos
 tv_type.y = tv_y_pos
-tv_type.width = screen_width / 3
+tv_type.width = tv_width
 tv_type.height = tv_height
 tv_type.allows_multiple_selection = True
 
@@ -221,11 +222,12 @@ tv_view = ui.TableView()
 tv_view.border_width = 0
 tv_view.x = 2 * tv_x_pos
 tv_view.y = 0
-tv_view.width = screen_width / 3
+tv_view.width = tv_width
 tv_view.height = tv_height
 
 # Button: generate chord
 bt_height = 40
+bt_width = screen_width
 bt_x_pos = 0
 bt_y_pos = tv_height + 5
 
@@ -234,13 +236,15 @@ bt_generator.border_width = 4
 bt_generator.title = 'Generate'
 bt_generator.x = bt_x_pos
 bt_generator.y = bt_y_pos
-bt_generator.width = screen_width
+bt_generator.width = bt_width
 bt_generator.height = bt_height
 bt_generator.font = ('verdana', 25)
 bt_generator.corner_radius = 10
 
 # TextView: amount about conversion
-txtv_height = screen_height - tv_height - tv_height - bt_height + 15  #280
+txtv_height = screen_height - tv_height - bt_height + 15  #280
+#txtv_width = screen_width - tv_width - bt_width + 15
+
 txtv_x_pos = 0
 txtv_y_pos = bt_y_pos + bt_height + 5
 
@@ -249,18 +253,20 @@ txtv_info.alignment = ui.ALIGN_CENTER
 txtv_info.border_width = 0
 txtv_info.x = txtv_x_pos
 txtv_info.y = txtv_y_pos
-txtv_info.width = screen_width
+txtv_info.width = screen_width  # txtv_width
 txtv_info.height = txtv_height
+
 txtv_info.editable = False
 txtv_info.font = ('verdana-bold', 30)
 
 
 class tvDelegateType(object):
-	def __init__(self, items):
+	def __init__(self, title, items):
 		self.items = items
-		self.selected_items = []
 		self.currentNumLines = len(items)
-		self.currentTitle = 'Type'
+		self.currentTitle = title
+		self.currentRow = None
+		self.selected_items = []
 
 	def tableview_did_select(self, tableview, section, row):
 		# Called when a row was selected.
@@ -270,39 +276,95 @@ class tvDelegateType(object):
 		# Called when a row was de-selected (in multiple selection mode).
 		self.selected_items.remove(self.items[row])
 
-
-class tvDataSourceAlter(object):
-	def __init__(self):
-		self.i = 1
-	
 	def tableview_number_of_sections(self, tableview):
-		# Return the number of sections (defaults to 1)
+		# Return the number of sections (defaults to 1). Someone else can mess with 
+		# sections and section logic
 		return 1
 
 	def tableview_number_of_rows(self, tableview, section):
 		# Return the number of rows in the section
-		return 3
-
-	def tableview_cell_for_row(self, tableview, section, row):
-		# Create and return a cell for the given section/row
-		self.i = self.i + 1
-		
-		cell = ui.TableViewCell()
-		cell.text_label.text = str(row)
-		return cell
-		
+		return self.currentNumLines  #needed to be in sync with displayed version, 
 
 	def tableview_title_for_header(self, tableview, section):
 		# Return a title for the given section.
 		# If this is not implemented, no section headers will be shown.
-		return 'Accedentals'
-		
-	def fill(self, tableview, data):
-		for section in data:
-			self.tableview_title_for_header(tableview, section)
-			
-			for ind, row in enumerate(data[section]):
-				self.tableview_cell_for_row(tableview, section, row)
+		return self.currentTitle
+
+	def tableview_cell_for_row(self, tableview, section, row):
+		# Create and return a cell for the given section/row
+
+		cell = ui.TableViewCell()
+		cell.text_label.text = self.items[row]['title']
+		cell.accessory_type = self.items[row]['accessory_type']
+		return cell
+
+
+class tvDelegateGen(
+		object
+):  #also acts as the data_source.  Can be separate, but this is easier.  
+	def __init__(self, title, items):
+		self.items = items
+		self.currentNumLines = len(items)
+		self.currentTitle = title
+		self.currentRow = None
+		self.selected_item = ''
+
+	def tableview_did_select(self, tableview, section, row):
+		# Called when a row was selected
+		self.selected_item = self.items[row]['title']
+
+	def tableview_did_deselect(self, tableview, section, row):
+		# Called when a row was de-selected (in multiple selection mode).
+		pass
+
+	def tableview_title_for_delete_button(self, tableview, section, row):
+		# Return the title for the 'swipe-to-***' button.
+		return 'Delete'  # or 'bye bye' or 'begone!!!'
+
+	def tableview_number_of_sections(self, tableview):
+		# Return the number of sections (defaults to 1). Someone else can mess with 
+		# sections and section logic
+		return 1
+
+	def tableview_number_of_rows(self, tableview, section):
+		# Return the number of rows in the section
+		return self.currentNumLines  #needed to be in sync with displayed version, 
+
+	def tableview_cell_for_row(self, tableview, section, row):
+		# Create and return a cell for the given section/row
+
+		cell = ui.TableViewCell()
+		cell.text_label.text = self.items[row]['title']
+		cell.accessory_type = self.items[row]['accessory_type']
+		return cell
+
+	def tableview_can_delete(self, tableview, section, row):
+		# Return True if the user should be able to delete the given row.
+		return True  # you can use logic to lock out specific ("pinned" entries) 
+
+	def tableview_can_move(self, tableview, section, row):
+		# Return True if a reordering control should be shown for the given row (in editing mode).
+		return True  # see above
+
+	def tableview_delete(self, tableview, section, row):
+		# Called when the user confirms deletion of the given row.
+		self.currentNumLines -= 1  # see above regarding hte "syncing"
+		tableview.delete_rows(
+			(row, ))  # this animates the deletion  could also 'tableview.reload_data()'
+		del self.items[row]
+
+	def tableview_move_row(self, tableview, from_section, from_row, to_section,
+																								to_row):
+		# Called when the user moves a row with the reordering control (in editing mode).
+
+		self.items = listShuffle(self.items, from_row, to_row)
+		# cynchronizes what is displayed with the underlying list
+
+	def tableview_title_for_header(self, tableview, section):
+		# Return a title for the given section.
+		# If this is not implemented, no section headers will be shown.
+		return self.currentTitle
+
 
 class ChordGenerator(ui.View):
 
@@ -311,43 +373,39 @@ class ChordGenerator(ui.View):
 	selected_type = ""
 	selected_types = []
 	selected_view = ""
+	chord_types = ' '
 
 	types = []
 
-	# определяем ноты
-	notes = []
-	all_notes = Note('C').all()
+	# ACCIDENTALS -----------------
+	titles = "b # all".split()
+	itemlist = [{'title': x, 'accessory_type': 'none'} for x in titles]
 
-	for note in all_notes:
-		notes.append(str(note))
+	tv_alter.data_source = tv_alter.delegate = tvDelegateGen(
+		title='Accidentals', items=itemlist)
 
-	data_src_notes = ui.ListDataSource(notes)
+	# CHORD TYPES -----------------
+	#for key, item in Chord(Note('C')).aliases.items():
+	#types.append(key)
 
-	# ListDataSource for accidentals
-	alter = {'Accidentals':['b', '#', 'all']}
-	
-	
-	# data_src_alter = ui.ListDataSource(alter)
-	
-	data_src_alter =  tvDataSourceAlter()
-	
-	data_src_alter.fill(tv_alter, alter)
-	
-	tv_alter.data_source = data_src_alter
-	tv_alter.delegate = data_src_alter
-	
-	
+	#data_src_type = ui.ListDataSource(types)
 
-	# определяем типы аккордов
 	for key, item in Chord(Note('C')).aliases.items():
-		types.append(key)
+		chord_types = chord_types + str(key) + ' '
 
-	data_src_type = ui.ListDataSource(types)
+	titles = chord_types.split()
+	itemlist = [{'title': x, 'accessory_type': 'none'} for x in titles]
 
-	# определяем как будет выглядеть аккорд
-	chord_views = ['Random', 'Default', 'Musthe']
+	tv_type.data_source = tv_type.delegate = tvDelegateType(
+		title='Chord type', items=itemlist)
 
-	data_src_view = ui.ListDataSource(chord_views)
+	# CHORD OUTPUT -----------------
+
+	titles = "Random Default Musthe".split()
+	itemlist = [{'title': x, 'accessory_type': 'none'} for x in titles]
+
+	tv_view.data_source = tv_view.delegate = tvDelegateGen(
+		title='Output', items=itemlist)
 
 	#-------------------------
 
@@ -365,16 +423,16 @@ class ChordGenerator(ui.View):
 		self.add_subview(txtv_info)
 
 		# Actions
-		self.data_src_alter.action = self.fn_alter_selected
+		#self.data_src_alter.action = self.fn_alter_selected
 
-		tv_type.data_source = self.data_src_type
+		#tv_type.data_source = self.data_src_type
 
-		tv_type.delegate = tvDelegateType(self.types)
+		#tv_type.data_source = tv_type.delegate = tvDelegateType(title = "Chord types", items = self.types)
 
 		# View
-		self.data_src_view.action = self.fn_view_selected
+		#self.data_src_view.action = self.fn_view_selected
 
-		tv_view.data_source = tv_view.delegate = self.data_src_view
+		#tv_view.data_source = tv_view.delegate = self.data_src_view
 
 		# Button: gnerate
 		bt_generator.action = self.fn_generate
@@ -395,13 +453,13 @@ class ChordGenerator(ui.View):
 		all_chords = []
 		new_chord = ''
 
-		if self.selected_alter == '':
+		if tv_alter.delegate.selected_item == '':
 			txtv_info.text = 'Select accidentals'
 
 		elif tv_type.delegate.selected_items == []:
 			txtv_info.text = 'Select chord type'
 
-		elif self.selected_view == '':
+		elif tv_view.delegate.selected_item == '':
 			txtv_info.text = 'Select output type'
 
 		else:
@@ -409,20 +467,20 @@ class ChordGenerator(ui.View):
 			for note in notes:
 				for item_type in tv_type.delegate.selected_items:
 
-					if self.selected_alter == 'all' or note.accidental == self.selected_alter or note.accidental == '':
+					if tv_alter.delegate.selected_item == 'all' or note.accidental == tv_alter.delegate.selected_item or note.accidental == '':
 
-						new_chord = str(Chord(note, chord_type=item_type))
+						new_chord = str(Chord(note, chord_type=item_type['title']))
 
 						# заменяем обозначения
-						if self.selected_view == 'Random':
+						if tv_view.delegate.selected_item == 'Random':
 
-							t = obj[item_type]
+							t = obj[item_type['title']]
 
 							new_chord = t.replace(new_chord)
 
-						elif self.selected_view == 'Default':
+						elif tv_view.delegate.selected_item == 'Default':
 
-							t = obj[item_type]
+							t = obj[item_type['title']]
 							new_chord = t.replace_default(new_chord)
 						all_chords.append(new_chord)
 
@@ -434,3 +492,4 @@ class ChordGenerator(ui.View):
 # -------------------------------
 v = ChordGenerator(name='CHORD GENERATOR')
 v.present('full_screen')
+
